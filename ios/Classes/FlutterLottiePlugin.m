26480e3552bad8ff6c03dde5ed64cdffc8be7dae
kaptnkoala <boehmer@lotum.de>
2022-09-06 21:14:04 +0200

    Fix names of flutter_lottie ios plugin

--- ios/Classes/FlutterLottiePlugin.m
+++ ios/Classes/FlutterLottiePlugin.m
@@ -1,5 +1,5 @@
 #import "FlutterLottiePlugin.h"
-#import <flutter_lottie_lotum/flutter_lottie_lotum-Swift.h>
+#import <flutter_lottie/flutter_lottie-Swift.h>
 
 @implementation FlutterLottiePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
--- lib/src/lottie_view.dart
+++ lib/src/lottie_view.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
@@ -70,38 +69,14 @@ class _LottieViewState extends State<LottieView> {
       "reverse": widget.reverse,
       "autoPlay": widget.autoPlay,
     };
-    if (defaultTargetPlatform == TargetPlatform.android) {
-      return PlatformViewLink(
-        viewType: viewType,
-        surfaceFactory: (BuildContext context, PlatformViewController controller) {
-          return AndroidViewSurface(
-            controller: controller as AndroidViewController,
-            gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
-            hitTestBehavior: PlatformViewHitTestBehavior.opaque,
-          );
-        },
-        onCreatePlatformView: (PlatformViewCreationParams params) {
-          final controller = PlatformViewsService.initExpensiveAndroidView(
-            id: params.id,
-            viewType: viewType,
-            layoutDirection: TextDirection.ltr,
-            creationParams: creationParams,
-            creationParamsCodec: const StandardMessageCodec(),
-            onFocus: () => params.onFocusChanged(true),
-          );
-          controller.addOnPlatformViewCreatedListener(params.onPlatformViewCreated);
-          controller.addOnPlatformViewCreatedListener(onPlatformViewCreated);
 
-          controller.create();
-          return controller;
-        },
-      );
-      /*return AndroidView(
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return AndroidView(
         viewType: viewType,
         creationParams: creationParams,
         creationParamsCodec: StandardMessageCodec(),
         onPlatformViewCreated: onPlatformViewCreated,
-      );*/
+      );
     } else if (defaultTargetPlatform == TargetPlatform.iOS) {
       return UiKitView(
         viewType: viewType,
